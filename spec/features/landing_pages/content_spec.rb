060cdcc35b62835811ba5fc53e2e67494ccd7662
Antonio Chavez <cavjzz@gmail.com>
2014-09-20 13:43:11 -0500

    Add style

--- spec/features/landing_pages/content_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-feature "Landing page content" do
-  scenario "Visiting the page" do
-    visit "/"
-
-    expect(page).to have_content("Facebook Pages Fetcher")
-  end
-end
