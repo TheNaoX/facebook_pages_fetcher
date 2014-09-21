b2a6cf732f7c0d3af79aca7bc2ebe8293b331af1
Antonio Chavez <cavjzz@gmail.com>
2014-09-21 12:27:16 -0500

    Add spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,16 @@ require 'rails_helper'
 describe User, "associations" do
   it { expect(subject).to have_many(:facebook_pages) }
 end
+
+describe User do
+  describe ".from_omniauth" do
+    let(:info) { double email: "email@example.com", name: "John", image: "http://google.com" }
+    let(:auth) { double provider: "facebook", uid: "12345678", info: info }
+
+    it "creates the user from omniauth" do
+      expect do
+        User.from_omniauth(auth)
+      end.to change(User, :count).by(1)
+    end
+  end
+end
