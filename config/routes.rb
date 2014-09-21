9dc6aab06dbecfd2e84afd3296ff0816eb0b487f
Antonio Chavez <cavjzz@gmail.com>
2014-09-21 13:52:38 -0500

    Fix router to see if it works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    only: [ :omniauth_callbacks, :sessions ],
-    controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
 
   resources :home, only: [:index] do
