6b969bfde2dd56f322f882c50eb3cbbcbf825ce6
Antonio Chavez <cavjzz@gmail.com>
2014-09-21 11:31:33 -0500

    Add association between users and facebook_pages

--- app/models/facebook_page.rb
+++ app/models/facebook_page.rb
@@ -6,12 +6,15 @@ class FacebookPage
   field :name,            type: String
   field :profile_picture, type: String
 
-  validates :facebook_id, :name, :profile_picture, presence: true
+  belongs_to :user
+
+  validates :facebook_id, :name, :profile_picture, :user, presence: true
   validates :facebook_id, uniqueness: true
 
-  def self.create_page(facebook_id, name, profile_picture)
+  def self.create_page(facebook_id, name, profile_picture, user_id)
     create!(facebook_id:     facebook_id,
             name:            name,
-            profile_picture: profile_picture)
+            profile_picture: profile_picture,
+            user_id:         user_id)
   end
 end
--- spec/features/lading_page_spec.rb
+++ spec/features/lading_page_spec.rb
@@ -4,6 +4,7 @@ feature "Landing page content" do
   scenario "Visiting the page" do
     visit "/"
 
-    expect(page).to have_content("Facebook Pages Fetcher")
+    expect(page).to have_content("FetcherApp")
+    expect(page).to have_content("Please sign in with Facebook to get started")
   end
 end
