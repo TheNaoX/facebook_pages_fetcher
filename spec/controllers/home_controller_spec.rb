79bf043fba291fe0688cd26349ada27361d50d42
Antonio Chavez <cavjzz@gmail.com>
2014-09-21 11:21:49 -0500

    Add spec for redirection

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 describe HomeController do
   describe "#index" do
-    it "renders the index successfully" do
-      get :index
-      expect(response).to be_successful
+    context "when user is signed in" do
+      let(:user) { create(:user) }
+      before { sign_in user }
+      it "renders the index successfully" do
+        get :index
+        expect(response).to be_successful
+      end
+    end
+
+    context "when user is not signed in" do
+      it "renders the index successfully" do
+        get :index
+        expect(request).to redirect_to(enter_home_index_path)
+      end
     end
   end
 end
