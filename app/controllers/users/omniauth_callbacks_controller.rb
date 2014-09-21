310d2473c448b50030c85e850eab725e9c67d206
Antonio Chavez <cavjzz@gmail.com>
2014-09-21 11:38:54 -0500

    Refactored controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,17 +1,26 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+      return session_success if user.persisted?
+      session_error
+    end
 
-      if @user.persisted?
-        sign_in @user
-        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-        redirect_to root_path
-      else
-        session["devise.facebook_data"] = request.env["omniauth.auth"]
-        flash[:alert] = "There was an error with your facebook login." if is_navigational_format?
-        redirect_to root_path
-      end
+    def session_success
+      sign_in user
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      redirect_to root_path
+    end
+
+    def session_error
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      flash[:alert] = "There was an error with your facebook login." if is_navigational_format?
+      redirect_to root_path
+    end
+
+    private
+
+    def user
+      @user ||= User.from_omniauth(request.env["omniauth.auth"])
     end
   end
 end
